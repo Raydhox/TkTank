d8de38908255238dec5e1cdb24ea473ea5280a63
Raydhox <38498323+Raydhox@users.noreply.github.com>
2018-05-09 19:30:59 +0200

    Update TkT4nk.py

--- TkT4nk.py
+++ TkT4nk.py
@@ -5,7 +5,7 @@
 #Par: UNG Harry
 #Description: Jeu de char utilisant tkinter. On dirige un char,
 #   et il faut exterminer tous les autres chars.
-#Version: 0.8 (Pour que je puisse me repérer).
+#Version: 0.78 (Pour que je puisse me repérer).
 #Idée d'amélioration: Une meilleure IA; un mode réseau; Pygame: bande sonore; support manette.
 #License: License libre
 #==================================================================================================
